da6dae06875c18cac514286f8a887ccdca3030c2
0n1dev <all_mind@naver.com>
2021-06-27 01:15:33 +0900

    feat : 디스코드 봇 토큰 로그인
    - XccBotConfig 추가

---
+++ src/main/java/kr/xcc/xccbot/config/XccBotConfig.java
@@ -0,0 +1,40 @@
+package kr.xcc.xccbot.config;
+
+import javax.security.auth.login.LoginException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+
+@Configuration
+public class XccBotConfig {
+
+	@Bean
+	JDA jda(@Value("${jda.token}") final String jdaToken) {
+		final JDABuilder builder = JDABuilder.createDefault(jdaToken);
+
+		builder.setActivity(Activity.watching("KorPUBG vBeta"));
+		builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
+		builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+		builder.setChunkingFilter(ChunkingFilter.NONE);
+		builder.setLargeThreshold(250);
+
+		try {
+			final JDA jda = builder.build();
+			jda.awaitReady();
+			return jda;
+		} catch (final LoginException e) {
+			e.printStackTrace();
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
