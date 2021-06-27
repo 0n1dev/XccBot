71b59d56145a807549e7eafe6f72dadda5bdee68
0n1dev <all_mind@naver.com>
2021-06-27 14:21:06 +0900

    fix : 웹훅 실패 테스트

--- src/main/java/kr/xcc/xccbot/XccBotApplication.java
+++ src/main/java/kr/xcc/xccbot/XccBotApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XccBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XccBotApplication.class, args);
+	SpringApplication.run(XccBotApplication.class, args);
 	}
 
 }
